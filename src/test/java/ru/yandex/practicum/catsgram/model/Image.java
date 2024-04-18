8b110b82b165a9a56619a17aa14b2b652e7dd512
Evgeniy <cokrat@yandex.ru>
2024-04-18 14:43:38 +0300

    Добавить возможность загрузки и скачивания изображения

--- src/test/java/ru/yandex/practicum/catsgram/model/Image.java
+++ src/test/java/ru/yandex/practicum/catsgram/model/Image.java
@@ -1,16 +1,11 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(of = "id")
 public class Image {
-    private   Long id;
-    private  long postId;
-
-    private  String originalFileName;
-    private  String filePath;
-
+    private Long id;
+    private long postId;
+    private String originalFileName;
+    private String filePath;
 }
-
